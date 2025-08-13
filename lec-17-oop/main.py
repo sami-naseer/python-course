7afff799626ebdbf7408a22403ca7fe478210d03
sami-naseer <ksamee601@gmail.com>
2025-08-13 11:33:59 +0500

    inheritance in oop

--- lec-17-oop/main.py
+++ lec-17-oop/main.py
@@ -3,10 +3,24 @@ class Car:#class name
         self.mybrand=brand#attribute/variables
         self.mymodel=model
         
-    def sum(a,b):#methods/function class ky andr ky functions ko methods kaha jata hy or variables ko attribute kaha jata hy
-        print( a+b)
+    def sum(self, a, b):  # method to sum two numbers
+        self.a = a
+        self.b = b
+        print( self.a + self.b)
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size=battery_size
         
-#my_car is object which store instance(adress) of class
-my_car = Car("toyota","corola")#
-print(my_car.mybrand)#here we call attribute of constructor
-print(my_car.mymodel)
+        
+# my_car is object which stores instance (address) of class
+my_car = Car("toyota", "corola")#object
+# print(my_car.mybrand)  # print brand attribute
+# print(my_car.mymodel)  # print model attribute
+# my_car.sum(12, 4)  # print sum of 12 and 4
+
+electric_car=ElectricCar("Tesla","S3","large")
+print(electric_car.mybrand)   # print brand of electric car
+print(electric_car.mymodel)   # print model of electric car
+print(electric_car.battery_size)
