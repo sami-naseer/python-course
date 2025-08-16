c8dde2f8f0fe774145b7f1c8e9888f82d376db42
sami-naseer <ksamee601@gmail.com>
2025-08-16 11:25:03 +0500

    polymorphism

---
+++ lec-17-oop/polymorphism.py
@@ -0,0 +1,41 @@
+#same name but differet beHAVIOUR(polymorphism)
+
+class Car:#class name
+    def __init__(self,brand,model):#constructor
+        self.__mybrand=brand#attribute/variables
+        self.mymodel=model
+    
+    def get_brand(self):
+        return self.__mybrand
+    
+    def sum(self, a, b):  # method to sum two numbers
+        self.a = a
+        self.b = b
+        print( self.a + self.b)
+        
+    def fuel_type(self):
+        print("petrol or disel")
+
+class ElectricCar(Car):
+    def __init__(self,brand,model,battery_size):
+        super().__init__(brand,model)
+        self.battery_size=battery_size
+        
+    def fuel_type(self):
+        print("electric charge")
+        
+        
+# my_car is object which stores instance (address) of class
+my_car = Car("toyota", "corola")#object
+my_car.fuel_type()
+# print(my_car.mybrand)  # print brand attribute
+# print(my_car.__mybrand)  # mybrand aik private attribute hy is liye hum class sy bahir access nahi kr skty           v
+# print(my_car.get_brand())
+# print(my_car.mymodel)  # print model attribute
+# my_car.sum(12, 4)  # print sum of 12 and 4
+
+electric_car=ElectricCar("Tesla","S3","large")
+electric_car.fuel_type()
+# print(electric_car._mybrand)   # print brand of electric car
+# print(electric_car.mymodel)   # print model of electric car
+# print(electric_car.battery_size)
